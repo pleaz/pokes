06a3bc55b8813e31b177bfe412b517c526310013
pleaz <oprstfaq@gmail.com>
2018-04-14 00:10:26 +0300

    404

--- resources/views/layouts/error.blade.php
+++ resources/views/layouts/error.blade.php
@@ -12,36 +12,15 @@
 
     <!-- Styles -->
     <style>
-        html, body {
-            background-color: #fff;
-            color: #636b6f;
-            font-family: 'Raleway', sans-serif;
-            font-weight: 100;
-            height: 100vh;
-            margin: 0;
-        }
-
-        .full-height {
-            height: 100vh;
-        }
-
-        .flex-center {
-            align-items: center;
-            display: flex;
-            justify-content: center;
-        }
-
-        .position-ref {
-            position: relative;
-        }
-
-        .content {
-            text-align: center;
-        }
-
-        .title {
-            font-size: 36px;
-            padding: 20px;
+        html, body {width:100%;height:100%;overflow:hidden;margin:0;padding:0;font-family:'Raleway',sans-serif;font-size:16px}
+        body {background:url('/404.png') top right no-repeat #fff}
+        .title {font-size:36px;padding:20px;}
+        .content {width:100%;text-align:center;position:absolute;bottom:10%;left:0;}
+        .content a {display:inline-block;text-decoration:none;font-size:14px;color:#2225e4;}
+        .content a:hover {color:#d02c2a;}
+        @media only screen and (max-width: 460px), screen and (max-height: 700px) {
+            .content {position:static;}
+            .content a {display:block;width:100%;height:100%;position:absolute;top:0;left:0;font-size:0;opacity:0;}
         }
     </style>
 </head>
@@ -49,6 +28,7 @@
 <div class="flex-center position-ref full-height">
     <div class="content">
         <div class="title">@yield('message')</div>
+        <a href="/">{{__('messages.pokes')}}</a>
     </div>
 </div>
 </body>
